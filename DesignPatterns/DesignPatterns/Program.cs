cecac1cda589fd7175406fa81dc12ed58bef7355
pekkamakkonen <pek.makkonen@gmail.com>
2020-12-30 01:04:06 +0200

    Classes

---
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("Pekka");
+            user.SayHello();
+            // Console.WriteLine(user.Name);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns
+{
+    class User
+    {
+        // Fields (attributes)
+        public String Name;
+        
+        public User(String name)
+        {
+            Name = name;
+        }    
+        
+        // Methods
+        public void SayHello()
+        {
+            Console.WriteLine($"Hei, minun nimeni on {Name}.");
+        }
+    }
+}
